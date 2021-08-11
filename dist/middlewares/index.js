d3dc6d3a2e579bc965d9980a467b55bbeaaff3e2
Rahul Ravindran <rahulr1116@gmail.com>
2021-08-11 08:12:45 +0530

    fix: Removed dist folder

--- dist/middlewares/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorHandler = exports.uploadFile = void 0;
-var multer_middleware_1 = require("./multer.middleware");
-Object.defineProperty(exports, "uploadFile", { enumerable: true, get: function () { return multer_middleware_1.uploadFile; } });
-var error_middleware_1 = require("./error.middleware");
-Object.defineProperty(exports, "errorHandler", { enumerable: true, get: function () { return error_middleware_1.errorHandler; } });
